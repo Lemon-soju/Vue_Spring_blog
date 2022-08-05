35c76f52637b62e861cf57e8eaaacb10dc169a71
Lemon Soju <yckk980320@naver.com>
2022-08-05 14:00:22 +0900

    Post Save Function-01

---
+++ src/main/java/com/example/lemonsoju_blog/domain/Post.java
@@ -0,0 +1,28 @@
+package com.example.lemonsoju_blog.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
