8f3f88c6e70ef9d3bd7fc932d1022f171fefbbd0
Lemon Soju <yckk980320@naver.com>
2022-08-05 17:21:34 +0900

    Post Save Function-02

--- src/main/java/com/example/lemonsoju_blog/controller/PostController.java
+++ src/main/java/com/example/lemonsoju_blog/controller/PostController.java
@@ -1,19 +1,15 @@
 package com.example.lemonsoju_blog.controller;
 
+import com.example.lemonsoju_blog.domain.Post;
 import com.example.lemonsoju_blog.request.PostCreate;
 import com.example.lemonsoju_blog.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -23,8 +19,7 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate request) {
+    public void post(@RequestBody @Valid PostCreate request) {
         postService.write(request);
-        return Map.of();
     }
 }
--- src/main/java/com/example/lemonsoju_blog/request/PostCreate.java
+++ src/main/java/com/example/lemonsoju_blog/request/PostCreate.java
@@ -1,5 +1,6 @@
 package com.example.lemonsoju_blog.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,4 +18,10 @@ public class PostCreate {
 
     @NotBlank(message = "콘텐츠를 입력해주세요.")
     private String content;
+
+    @Builder
+    public PostCreate(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
