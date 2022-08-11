c82c045199633db16c27f51c5bd73ab62c4efe41
Lemon Soju <yckk980320@naver.com>
2022-08-11 13:21:21 +0900

    Separating Board Response Class

--- src/main/java/com/example/lemonsoju_blog/service/PostService.java
+++ src/main/java/com/example/lemonsoju_blog/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.lemonsoju_blog.service;
 import com.example.lemonsoju_blog.domain.Post;
 import com.example.lemonsoju_blog.repository.PostRepository;
 import com.example.lemonsoju_blog.request.PostCreate;
+import com.example.lemonsoju_blog.response.PostResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,9 +25,16 @@ public class PostService {
         postRepository.save(post);
     }
 
-    public Post get(Long id) {
+    public PostResponse get(Long id) {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 글입니다."));
-        return post;
+
+        PostResponse response = PostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .build();
+
+        return response;
     }
 }
