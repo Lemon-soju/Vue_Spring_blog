67e89498fde5d403de7f1dd44f450dbb896ad3f7
Lemon Soju <yckk980320@naver.com>
2022-08-27 12:44:41 +0900

    Delete Posts

--- src/main/java/com/example/lemonsoju_blog/controller/PostController.java
+++ src/main/java/com/example/lemonsoju_blog/controller/PostController.java
@@ -41,5 +41,10 @@ public class PostController {
     public PostResponse edit(@PathVariable Long postId, @RequestBody @Valid PostEdit request) {
         return postService.edit(postId, request);
     }
+
+    @DeleteMapping("/posts/{postId}")
+    public void delete(@PathVariable Long postId) {
+        postService.delete(postId);
+    }
 }
 
