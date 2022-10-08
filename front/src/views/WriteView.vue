5150cf8d90c50334cb56a7d30c23271c33cd11c0
Lemon Soju <yckk980320@naver.com>
2022-10-09 02:01:34 +0900

    Make Post List Page

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -2,16 +2,22 @@
 import {ref} from "vue";
 
 import axios from 'axios';
+import {useRouter} from "vue-router";
 
-const title = ref("")
-const content = ref("")
+const title = ref("");
+const content = ref("");
+
+const router = useRouter();
 
 const write = function () {
   axios.post("/lemonsoju-api/posts", {
     title: title.value,
     content: content.value
-  });
-}
+  })
+      .then(() => {
+        router.replace({ name: "home"})
+      })
+};
 </script>
 
 <template>
